dcbf50338570f60ca6a2ce2551d22631a84caa02
tinannan <yashenkoalevtina@gmail.com>
2024-12-09 09:25:14 +0200

    initial commit

--- app/src/main/java/com/example/lab1209/MainActivity.java
+++ app/src/main/java/com/example/lab1209/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab1209;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView itemsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        itemsListView = findViewById(R.id.listView);
     }
 }
